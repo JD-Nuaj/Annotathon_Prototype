b93948eef9203c8d47bed68f7d0a043ae7db5468
juan descoux <juan.descoux@hotmail.fr>
2025-01-30 13:27:48 +0100

    Navbar debugg

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,9 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["searchBar", "searchIcon"]
+  static targets = ["searchBar", "searchIcon", "mobileMenu"]
 
   toggleSearch() {
     this.searchBarTarget.classList.toggle("hidden")
+  }
+
+  toggleMobileMenu() {
+    this.mobileMenuTarget.classList.toggle("hidden")  // Bascule l'affichage du menu mobile
   }
 }
